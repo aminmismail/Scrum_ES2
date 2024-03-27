ae00bd2843f1a0b759f6c3bfbbbb158d8fad7b02
Coconutoos <gteixeiraayala@gmail.com>
2024-03-27 00:39:40 -0300

    feat(profissional): implementado Entity profissional

--- BackEnd/t1/src/main/java/com/es2/t1/services/ProfissionalService.java
+++ BackEnd/t1/src/main/java/com/es2/t1/services/ProfissionalService.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional; 
-
+ 
 @Service
 @AllArgsConstructor
 public class ProfissionalService {
